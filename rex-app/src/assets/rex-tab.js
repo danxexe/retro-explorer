2381c28ef19c44f6bd85589b90f2a50c70a59565
danxexe <danx.exe@gmail.com>
2026-01-04 06:34:57 +0900

    Move tab logic to component

---
+++ rex-app/src/assets/rex-tab.js
@@ -0,0 +1,148 @@
+import {LitElement, css, html} from '/assets/lit-core.min.js';
+
+export class RexTab extends LitElement {
+  static styles = css`
+    :host {
+      display: contents;
+    }
+
+    details {
+      display: contents;
+
+      &[open]::details-content {
+        display: contents;
+      }
+
+      & > summary {
+        grid-row: 1;
+
+        &::after {
+          display: none;
+        }
+      }
+
+      &[open] > section {
+        grid-row: 2;
+        grid-column: 1 / -1;
+        display: flex;
+        flex-direction: column;
+      }
+    }
+
+    details {
+      & > summary {
+        white-space: nowrap;
+        cursor: pointer;
+        padding: 0.5rem 1.5rem;
+        border-radius: 6px 6px 0 0;
+        text-align: center;
+        margin: 0;
+        border: 1px solid var(--pico-mark-background-color);
+        list-style: none;
+        line-height: 1rem;
+      }
+
+      &[open] {
+        & > summary {
+          --pico-accordion-open-summary-color: #fff;
+          background-color: var(--pico-mark-background-color);
+          pointer-events: none;
+        }
+      }
+
+      & > summary:hover {
+        background-color: var(--pico-secondary-background);
+      }
+
+      & > section {
+        margin: 0;
+        border-top: 1px solid var(--pico-primary-border);
+
+        & > iframe {
+          border: none;
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  `;
+
+  static properties = {
+    name: { type: String },
+    label: { type: String },
+    group: { type: String },
+    src: { type: String },
+    open: { type: Boolean, reflect: true },
+  };
+
+  constructor() {
+    super();
+    this.label = '';
+  }
+
+  onToggleHandler(e) {
+    const type = e.target.open ? 'toggleOn' : 'toggleOff';
+
+    const evt = new CustomEvent(type, {
+      composed: true,
+      bubbles: true,
+      cancelable: true,
+      detail: {
+        group: this.group,
+      },
+    });
+
+    this.dispatchEvent(evt);
+
+    if (evt.defaultPrevented) {
+      e.target.setAttribute('open', this.open);
+    } else {
+      this.open = e.target.open;
+    }
+  }
+
+  render() {
+    return html`
+      <details data-plugin="test" name="${this.group}" @toggle="${this.onToggleHandler}" ?open=${this.open}>
+        <summary>${this.label}</summary>
+        <section>
+          <iframe tabindex="-1" src="${this.src}"></iframe>
+        </section>
+      </details>
+    `
+  };
+
+  async connectedCallback() {
+    super.connectedCallback();
+
+    setTimeout(() => {
+      const iframe = this.renderRoot.querySelector('iframe');
+      const win = iframe.contentWindow;
+
+      win.addEventListener('keydown', function(e) {
+        win.parent.postMessage({
+            type: 'keydown',
+            key: e.key,
+            keyCode: e.keyCode,
+            ctrlKey: e.ctrlKey,
+            shiftKey: e.shiftKey,
+            altKey: e.altKey,
+            metaKey: e.metaKey,
+        }, '*');
+        e.preventDefault();
+      });
+
+      win.addEventListener('error', function(e) {
+        win.parent.postMessage({
+          type: 'error',
+          message: e.message,
+          filename: e.filename,
+          lineno: e.lineno,
+          colno: e.colno,
+        }, '*');
+      });
+    });
+  }
+}
+
+customElements.define('rex-tab', RexTab);
