08d418b7aca9ac998ae49dce122eb8c38ebc985c
danxexe <danx.exe@gmail.com>
2026-01-08 06:41:07 +0900

    Display collection files + basic search

---
+++ rex-app/src/assets/rex-collection-entries.js
@@ -0,0 +1,146 @@
+import {LitElement, css, html} from '/assets/lit-core.min.js';
+import picoCss from '/assets/pico.min.css' with { type: 'css' };
+import picoColorsCss from '/assets/pico.colors.min.css' with { type: 'css' };
+
+export class RexCollectionEntries extends LitElement {
+  static styles = [picoCss, picoColorsCss, css`
+  .tag {
+    background-color: var(--pico-color-slate-800);
+    font-size: 70%;
+    padding: 0rem 0.3rem 0.1rem;
+    border-radius: 0.3rem;
+
+    &.console {
+      background-color: var(--pico-color-violet-900);
+    }
+  }
+
+  ol {
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 1rem;
+  }
+
+  li {
+    list-style: none;
+
+    & article {
+      height: 100%;
+      margin: 0;
+      padding: 0rem 0.8rem 0.6rem;
+
+      & header {
+        margin: 0;
+        padding: 0;
+        background-color: transparent;
+      }
+    }
+  }
+  `];
+
+  static properties = {
+    entries: { type: Array },
+    // visibleEntries: { type: Array, state: true },
+  }
+
+  constructor() {
+    super();
+    this.entries = [];
+    // this.visibleEntries = [];
+  }
+
+  render() {
+    return html`
+    <input type="search" @input="${this.#handleSearch}" autofocus>
+    <ol>
+      ${this.entries.map(entry => this.#renderEntry(entry))}
+    </ol>
+    `
+  }
+
+  #renderEntry(entry) {
+    const [gameName, consoleName, tags] = this.#normalizeEntryName(entry.fs_path);
+
+    return html`
+    <li data-path="${entry.fs_path.toLowerCase()}">
+      <article>
+        <header>
+          <span class="tag console">${consoleName}</span>
+          ${tags.map(tag => html`
+            <span class="tag">${tag}</span>
+          `)}
+        </header>
+        <div>${gameName}</div>
+      </article>
+    </li>
+    `
+  }
+
+  /**
+   * TODO: move logic to Rust and persist
+   *
+   * @param {String} path
+   * @returns {[String, String, Array<String>]}
+   */
+  #normalizeEntryName(path) {
+    const parts = path.split('/');
+    const consoleName = parts[0];
+    const filename = parts[parts.length - 1];
+    const filenameParts = filename.split('.');
+    const ext = filenameParts.pop()
+    const basename = filenameParts.join('.');
+    const [gameName, tags] = this.#extractTags(basename);
+
+    return [gameName, consoleName, tags];
+  }
+
+  /**
+   * @param {String} filename
+   * @returns {[String, Array<String>]}
+   */
+  #extractTags(filename) {
+    const tagsPattern = /(.*?)\s+\(([^)]+)\)$/;
+    let tags = [];
+    let gameName = filename;
+    let matches = tagsPattern.exec(gameName);
+    while (matches !== null) {
+      let [_, rest, tag] = matches;
+      gameName = rest;
+      tags.unshift(tag);
+      matches = tagsPattern.exec(gameName);
+    }
+
+    return [gameName, tags];
+  }
+
+  #handleSearch(e) {
+    const words = e.target.value.split(' ');
+
+    const articles = this.shadowRoot.querySelectorAll('li');
+
+    articles.forEach(e => {
+      e.hidden = true;
+    });
+
+    const hidden = words.reduce((acc, word) => {
+      return acc.filter(article => article.dataset.path.includes(word));
+    }, [...articles]);
+
+    hidden.forEach(e => {
+      e.hidden = false;
+    });
+
+    // TODO: filter on state instead of DOM.
+    // Unfortunately, the following implementation uses more CPU than direct DOM manipulation.
+    // Should probably try lit's `repeat`.
+
+    // const visibleEntries = words.reduce((acc, word) => {
+    //   return acc.filter(entry => entry.fs_path.toLowerCase().includes(word));
+    // }, this.entries);
+
+    // this.visibleEntries = visibleEntries;
+  }
+}
+
+customElements.define('rex-collection-entries', RexCollectionEntries);
